8a2538cf3348499b95795f4171e89d222cc264ff
kurotamashi30 <kurotamashi30@gmail.com>
2024-04-28 20:52:14 +0900

    done2

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <div className='about-container'>
       <div className='about-content'>
-        <div className='about-content_t'><p>ホワイトボードのように投稿する内容を見れるアプリです。</p>
-        <p>ポスト投稿アプリは、ユーザーが簡単に文章を掲示できるプラットフォームです。</p></div>
+        <div className='about-content_t'>
+        <p>本アプリは、ユーザーが簡単に文章を掲示できるアプリです。</p></div>
       </div>
     </div>
   );
