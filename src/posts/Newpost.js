25e02130b57f878767aa285c62867cb2674bbfa5
kurotamashi30 <kurotamashi30@gmail.com>
2024-06-03 12:32:15 +0900

    Done URL

--- src/posts/Newpost.js
+++ src/posts/Newpost.js
@@ -1,6 +1,6 @@
 import { useState ,useEffect} from 'react';
 import {jwtDecode} from 'jwt-decode'; // Corrected import statement
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,useParams } from 'react-router-dom';
 import { useAuth } from '../components/AuthContext';
 import './Newpost.css';
 
@@ -14,13 +14,37 @@ function Newpost({ todos, setTodos}) {
   const [bodyCharacterCount, setBodyCharacterCount] = useState(0);
   const { currentUser ,setCurrentUser} = useAuth();
 
+  const jwtToken = localStorage.getItem('token')
+  let { user_id } = useParams();
   const navigate = useNavigate();
   const MAX_TITLE_LENGTH = 15;
   const MAX_BODY_LENGTH = 100;
   //let { user_id } = useParams();
 //testのため仮のトークンを入れます。本来ならいらないです。
-  //const jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3MDg3NTkyMTR9.IL8J7ngUjO5VPBJ4AoUOUWUGJzie_oS0JkoNePWBhVU"
-  //const user_id = 1;
+//const jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3MDg3NTkyMTR9.IL8J7ngUjO5VPBJ4AoUOUWUGJzie_oS0JkoNePWBhVU"
+//const user_id = 1;
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (currentUser && currentUser.jwtToken) {
+          const decodedToken = jwtDecode(jwtToken);
+          setUserName(decodedToken.name);
+        }
+      } catch (error) {
+        console.error('Failed to decode JWT:', error);
+      }
+    };
+    if (!jwtToken) {
+      navigate('/auth'); // Redirect to login page if user is not logged in
+    } else if (currentUser && user_id && Number(user_id) !== currentUser.id) {
+      alert("You are trying to access an invalid URL. Please correct it.");
+      navigate(`/users/${currentUser.id}/micropost`); // Redirect to the correct user's page
+    } else {
+      fetchData();
+    }
+  }, [currentUser,user_id, jwtToken, navigate,setCurrentUser]);
 
   const handleTitleChange = (e) => {
     const inputValue = e.target.value;
@@ -29,6 +53,7 @@ function Newpost({ todos, setTodos}) {
       setTitleCharacterCount(inputValue.length);
     }
   };
+
   const handleBodyChange = (e) => {
     const inputValue = e.target.value;
     if (inputValue.length <= MAX_BODY_LENGTH) {
@@ -36,62 +61,41 @@ function Newpost({ todos, setTodos}) {
       setBodyCharacterCount(inputValue.length);
     }
   };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!title.trim() || !body.trim()) {
-      setFlashMessage('投稿内容を入力してください。');  // You can also use a state variable to show an error message
+      setFlashMessage("Please enter your post content."); // Corrected string with escaped single quote
       return;
     }
+
     setFlashMessage('');
-    // Retrieve the JWT token from local storage
-    const jwtToken = localStorage.getItem('token');
-    if (!jwtToken) {
-      console.error('No JWT Token found, user might not be logged in');
-      return;
-    }
-    if (currentUser && currentUser.jwtToken) {
-    try {
-      const decodedToken = jwtDecode(jwtToken);
-      const user_id = decodedToken.user_id;
-       // Instead of declaring userName here, just assign it
-       setUserName(decodedToken.name);
-      const postData = {
-        title: title,
-        body: body,
-      };
 
-      //console.log('Sending postData:', postData);
-      const response = await fetch(`http://${process.env.REACT_APP_API_URL}:3000/api/v1/users/${user_id}/microposts`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${jwtToken}`
-        },
-        body: JSON.stringify(postData),
-      });
+    const postData = {
+      title: title,
+      body: body,
+    };
 
-      const data = await response.json();
-      //console.log('Response data:', data);
-      if (response.ok) {
-        setUserName(data.user.name);
-        navigate(`/users/${user_id}`);
-      } else {
-        throw new Error(`Error in creating post: ${data.message || "HTTP error! status: " + response.status}`);
-      }
-    } catch (error) {
-      console.error('Failed to decode JWT or perform POST request:', error);
+    const response = await fetch(`http://${process.env.REACT_APP_API_URL}:3000/api/v1/users/${user_id}/microposts`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${jwtToken}`
+      },
+      body: JSON.stringify(postData),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      setUserName(data.user.name);
+      navigate(`/users/${user_id}`);
+    } else {
+      throw new Error(`Error in creating post: ${data.message || "HTTP error! status: " + response.status}`);
     }
-  }
-};
-useEffect(() => {
-  const jwtToken = localStorage.getItem('jwtToken');
-  if (!currentUser && jwtToken) {
-    setCurrentUser(JSON.parse(localStorage.getItem('currentUser')));
-  } else if (!jwtToken) {
-    navigate('/auth');
-  }
-}, [currentUser, navigate, setCurrentUser]); 
-    return (
+  };
+
+return (
       <div className="post_body_main ">
   <div className="post_body_top">
     <p>{userName ? `${userName}様の投稿ページ` : ''}</p>
